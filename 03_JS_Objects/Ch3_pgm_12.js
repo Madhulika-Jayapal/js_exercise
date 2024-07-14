19d36b599a3fed538fffdee4d9cad18289cdc4a8
Madhulika Jayapal <madhulikajayapal2@gmail.com>
2024-07-14 15:35:24 +0530

    Update Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,7 +8,23 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+var post2 = {
+	id : 2,
+	title : "Be Loved",
+	author : "Emma Mia",
+	created : "1989-06-21",
+	body : "The way of love"
+};
 
+console.log("post Title:", post.title);
+console.log("Author:", post.author);
+console.log("created:", post.created);
+console.log("post Body:" post.body);
+
+console.log("post Title:", post2.title);
+console.log("Author:", post2.author);
+console.log("created:", post2.created);
+console.log("post Body:" post2.body);
 
 /* Further Adventures
  *
